f3fb4a572a5c3c4b001a284a98e572840a3cd9a1
Yuriy Tumakha <tumakha@gmail.com>
2026-04-17 13:41:27 +0100

    BST-161685 Upgraded test libs

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -4,14 +4,16 @@ import sbt.*
 private object LibDependencies {
 
   val bootstrapVersion               = "10.7.0"
-  val scalaTestPlusScalaCheckVersion = "3.2.19.0"
-  val scalaTestPlusMockitoVersion    = "3.2.19.0"
+  val scalaTestVersion               = "3.2.20"
+  val scalaTestPlusScalaCheckVersion = "3.2.20.0"
+  val scalaTestPlusMockitoVersion    = "3.2.20.0"
 
   val unitTestDependencies: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion % Provided,
     "org.apache.pekko"  %% "pekko-testkit"          % PlayVersion.pekkoVersion,
-    "org.scalatestplus" %% "scalacheck-1-18"        % scalaTestPlusScalaCheckVersion,
-    "org.scalatestplus" %% "mockito-5-12"           % scalaTestPlusMockitoVersion
+    "org.scalatest"     %% "scalatest"              % scalaTestVersion,
+    "org.scalatestplus" %% "scalacheck-1-19"        % scalaTestPlusScalaCheckVersion,
+    "org.scalatestplus" %% "mockito-5-23"           % scalaTestPlusMockitoVersion
   )
 
   val integrationTestDependencies: Seq[ModuleID] = unitTestDependencies
